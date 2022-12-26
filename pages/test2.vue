16007e30afbd47810ace58bd8471e006d2b4e75f
mahanteshaCloudMPower <mahantesha@cloudmpower.com>
2022-12-26 18:12:54 +0530

    changes

---
+++ pages/test2.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="flex ...">
+  <div class="flex-none ...">01</div>
+  <!-- <div class="contents"> -->
+    <div class="flex-1 ...">02</div>
+    <div class="flex-1 ...">03</div>
+  <!-- </div> -->
+  <div class="flex-1 ...">04</div>
+</div>
+<div class="bg-white shadow-xl rounded-xl mt-10">
+<div class="table w-full ">
+  <div class="table-header-group  ">
+    <div class="table-row bg-gray-500 rounded-xl px-2 py-2   ">
+      <div class="table-cell text-left ...">Song</div>
+      <div class="table-cell text-left ...">Artist</div>
+      <div class="table-cell text-left ...">Year</div>
+    </div>
+  </div>
+  <div class="table-row-group">
+    <div class="table-row">
+      <div class="table-cell ...">The Sliding Mr. Bones (Next Stop, Pottersville)</div>
+      <div class="table-cell ...">Malcolm Lockyer</div>
+      <div class="table-cell ...">1961</div>
+    </div>
+    <div class="table-row">
+      <div class="table-cell ...">Witchy Woman</div>
+      <div class="table-cell ...">The Eagles</div>
+      <div class="table-cell ...">1972</div>
+    </div>
+    <div class="table-row">
+      <div class="table-cell ...">Shining Star</div>
+      <div class="table-cell ...">Earth, Wind, and Fire</div>
+      <div class="table-cell ...">1975</div>
+    </div>
+  </div>
+  <div class="table-footer-group">
+
+    <div class="table-row  sm:hidden ">
+        <div class="table-cell">table foot </div>
+        <div class="table-cell">table foot </div>
+        <div class="table-cell">table foot </div>
+    </div>
+  </div>
+</div>
+</div>
+<div class="flex ...">
+  <div class="lg:hidden md:flex-1 ...">01</div>
+  <div>02</div>
+  <div>03</div>
+</div>
+<div class="flex flex-nowrap ">
+  <div class="bg-gray-500">01</div>
+  <div class="bg-gray-500">02</div>
+  <div class="bg-gray-500">03</div>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
